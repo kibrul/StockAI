f1fd1ea8e1ca0b4c8e1c5451d9c5d9946ca4366b
Kibrul Bary Tani <59243774+kibrul@users.noreply.github.com>
2025-10-29 13:04:19 +0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,6 +5,6 @@ from streamlit_extras.app_logo import add_logo
 def init():
     add_logo(
        # "https://raw.githubusercontent.com/andfanilo/social-media-tutorials/master/20221130-extras/logo.png"
-        "h:/stock_analysis/tlogo.png"
+        "tlogo.png"
     )
     #button(username="andfanilo", floating=True, width=222)
